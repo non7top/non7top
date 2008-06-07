db6af4d5222035588496ec43ef7d32e1ea07d54e
non7top <non7top@8ae7c38d-f53e-0410-b390-bd530aee3462>
2008-06-07 17:35:39 +0000

    add utils

--- trunk/python/lib/utils.py
+++ trunk/python/lib/utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+
+
 ############################################################################
 #    Copyright (C) 2007 by non7top   #
 #    non7top@gmail.com   #
@@ -18,4 +20,35 @@
 #    Free Software Foundation, Inc.,                                       #
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
+
+import sys
+import os
+import base64
+import re
+import urllib
+import urllib2
+from StringIO import StringIO
+
+from lxml import etree
+
+
+'''return number from text'''
+def getnum(txt):
+    n = re.findall(r"\d+",txt)[0]
+    return n
+	
+def dw_wapage(wa_addr):
+    parser = etree.HTMLParser()
+    
+    req = urllib2.Request(wa_addr)
+
+    try:
+        handle = urllib2.urlopen(req)
+    except IOError:
+        print "ioerror"
+
+    thepage = unicode( handle.read(),    "cp1251")
+    tree = etree.parse(StringIO(thepage), parser)
+    handle.close()
+    return thepage, tree
 
